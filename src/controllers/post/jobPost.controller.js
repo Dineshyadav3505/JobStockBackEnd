31059419a1d76462366e9d1d592fd1802ab45fd0
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-07-07 08:54:26 +0530

    Add multer middleware for file uploads

--- src/controllers/post/jobPost.controller.js
+++ src/controllers/post/jobPost.controller.js
@@ -5,58 +5,129 @@ import { ApiResponse } from "../../utils/ApiResponse.js";
 
 
 const createJobPost = asyncHandler(async (req, res) => {
+    const { 
+        postName, 
+        postDescription, 
+        date1, 
+        date2, 
+        date3, 
+        date4, 
+        date5, 
+        date6, 
+        date7, 
+        date8, 
+        date9, 
+        date10, 
+        Fee1, 
+        Fee2, 
+        Fee3, 
+        Fee4, 
+        Fee5, 
+        Fee6, 
+        Fee7, 
+        Fee8, 
+        Fee9, 
+        Fee10, 
+        age1, 
+        age2, 
+        age3, 
+        age4, 
+        age5, 
+        age6, 
+        age7, 
+        age8, 
+        age9, 
+        age10, 
+        totalPost, 
+        iconImage, 
+        postImage, 
+        applyLink 
+    } = req.body;
+
     const user = req.user;
 
     if (user.role !== "Admin") {
         throw new ApiError(401, "Unauthorized");
     }
-
-    const { postName, postDescription, date1, date2, date3, date4, date5, date6, date7, date8, date9, date10, Fee1, Fee2, Fee3, Fee4, Fee5, Fee6, Fee7, Fee8, Fee9, Fee10, age1, age2, age3, age4, age5, age6, age7, age8, age9, age10, totalPost, iconImage, postImage, applyLink } = req.body;
     
-    console.log(postName, postDescription, totalPost, iconImage, postImage, applyLink);
-
     if ([postName, postDescription, totalPost, iconImage, postImage, applyLink].some((field) => field?.trim() === "")) {
         throw new ApiError(400, "All fields are required");
     }
-    
-    const jobPost = await JobPost.create({
-        postName,
-        postDescription,
-        date1,
-        date2,
-        date3,
-        date4,
-        date5,
-        date6,
-        date7,
-        date8,
-        date9,
-        date10,
-        Fee1,
-        Fee2,
-        Fee3,
-        Fee4,
-        Fee5,
-        Fee6,
-        Fee7,
-        Fee8,
-        Fee9,
-        Fee10,
-        age1,
-        age2,
-        age3,
-        age4,
-        age5,
-        age6,
-        age7,
-        age8,
-        age9,
-        age10,
-        totalPost,
-        iconImage,
-        postImage,
-        applyLink
-    });
+
+    if (!req.files || !req.files.iconImage || !req.files.postImage){
+        throw new ApiError(400, "Product image is required");
+    }
+
+    const iconImageLocalPaths = req.files.iconImage.map(file => file.path);
+    const postImageLocalPaths = req.files.postImage.map(file => file.path);
+
+    if (!iconImageLocalPaths || iconImageLocalPaths.length === 0) {
+        throw new ApiError(400, "iconImage file path is required");
+    }
+
+    if (!postImageLocalPaths || postImageLocalPaths.length === 0) {
+        throw new ApiError(400, "postImage file path is required");
+    }
+
+    try {
+        const iconImage = await Promise.all(iconImageLocalPaths.map(async (path) => {
+            const result = await uploadOnCloudinary(path);
+            return result.secure_url;  // Assuming 'uploadOnCloudinary' returns an object with 'secure_url'
+        }));
+        const postImage = await Promise.all(postImageLocalPaths.map(async (path) => {
+            const result = await uploadOnCloudinary(path);
+            return result.secure_url;  // Assuming 'uploadOnCloudinary' returns an object with 'secure_url'
+        }));
+
+        const jobPost = await JobPost.create({
+            postName,
+            postDescription,
+            date1,
+            date2,
+            date3,
+            date4,
+            date5,
+            date6,
+            date7,
+            date8,
+            date9,
+            date10,
+            Fee1,
+            Fee2,
+            Fee3,
+            Fee4,
+            Fee5,
+            Fee6,
+            Fee7,
+            Fee8,
+            Fee9,
+            Fee10,
+            age1,
+            age2,
+            age3,
+            age4,
+            age5,
+            age6,
+            age7,
+            age8,
+            age9,
+            age10,
+            totalPost,
+            iconImage,
+            postImage,
+            applyLink
+        });
+
+        return res
+        .status(201)
+        .json(new ApiResponse(
+            201, 
+            jobPost,
+            "Post created successfully"
+        ));  
+    } catch (error) {
+        throw new ApiError(400, "Error uploading image");
+    }
 })
 
 const getJobPosts = asyncHandler(async (req, res) => {
--- src/routes/post/jobPost.route.js
+++ src/routes/post/jobPost.route.js
@@ -1,11 +1,23 @@
 import {createJobPost, getJobPosts, getJobPostById, updateJobPost, deleteJobPost} from '../../controllers/post/jobPost.controller.js';
 import { Router } from "express";
+import { upload } from "../../middlewares/multer.middleware.js";
 import {verifyJWT} from "../../middlewares/auth.middleware.js";
 
 const router = Router()
 
 
-router.route("/create").post(verifyJWT, createJobPost)
+router.route("/create").post(
+    upload.fields([
+        {
+            name : "iconImage",
+
+        },
+        {
+            name : "postImage"
+        }
+    ]),
+    
+    verifyJWT, createJobPost)
 router.route("/job").get(getJobPosts)
 router.route("/:id").get(getJobPostById)
 router.route("/update").patch(verifyJWT, updateJobPost)
