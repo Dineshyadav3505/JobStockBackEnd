fe3b1e88d8b643a2cef08396757e75006f029312
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-07-07 22:24:35 +0530

    Refactor dataConnection.js to improve code readability and fix connection issue

--- src/db/dataConnection.js
+++ src/db/dataConnection.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
- 
 
-const connectToDatabase = async(req, res )=>{
+const connectToDatabase = async (req, res) => {
   try {
-       const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
-       console.log(`MongoDB Connected !! DB HOST: ${connectionInstance.Connection.host}`);
+    mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`, );
+    console.log(`MongoDB Connected !! DB HOST: ${mongoose.connection.host}`);
 
-   } catch (error) {
-       console.error("MONGO-DB Connection Error:", error);
-       process.exit(1);
-   }
+  } catch (error) {
+    console.error("MONGO-DB Connection Error:", error);
+    process.exit(1);
+  }
 };
-export {connectToDatabase}
+
+export { connectToDatabase };
